c2f37e657bfaae7bf9916096a822ed5eff57be13
Lakitha Madanayake <lakitha1999@gmail.com>
2021-09-24 10:06:44 +0530

    Add scroll function to the app

--- budjetapp/lib/main.dart
+++ budjetapp/lib/main.dart
@@ -21,20 +21,22 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.lightBlue,
-              child: Text('CHART!'),
-              elevation: 5,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.lightBlue,
+                child: Text('CHART!'),
+                elevation: 5,
+              ),
             ),
-          ),
-        UserTransactions(),
-        ],
+          UserTransactions(),
+          ],
+        ),
       ),
     );
   }
