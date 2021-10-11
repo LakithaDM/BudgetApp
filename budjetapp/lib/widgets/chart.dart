c9b00056c2d9bb709ef9c982b089880a22341afb
Lakitha Madanayake <lakitha1999@gmail.com>
2021-10-12 03:56:02 +0530

    Add chart widget

---
+++ budjetapp/lib/widgets/chart.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import '../models/Transaction.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekday = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekday.day &&
+            recentTransactions[i].date.month == weekday.month &&
+            recentTransactions[i].date.year == weekday.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+
+      print(DateFormat.E().format(weekday));
+      print(totalSum);
+
+      return {
+        'day': DateFormat.E().format(weekday),
+        'amount': totalSum,
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(groupedTransactionValues);
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
