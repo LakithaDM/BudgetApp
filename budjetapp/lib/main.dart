d9c57ebd5f7aae6095f74a77bae7d155bd433968
Lakitha Madanayake <lakitha1999@gmail.com>
2021-10-26 01:01:11 +0530

    Make the UI adaptive for IOS users

--- budjetapp/lib/main.dart
+++ budjetapp/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -179,7 +180,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton:Platform.isIOS ? Container() : FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () => _startAddNewTransaction(context),
       ),
