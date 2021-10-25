348aac6ea8a209f589c34965653483e9651a3b6f
Lakitha Madanayake <lakitha1999@gmail.com>
2021-10-26 00:57:06 +0530

    Make the toggle switch adaptive for different OS's

--- budjetapp/lib/main.dart
+++ budjetapp/lib/main.dart
@@ -147,7 +147,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text('Show Chart'),
-                  Switch(
+                  Switch.adaptive(
                     value: _showChart,
                     onChanged: (val) {
                       setState(() {
